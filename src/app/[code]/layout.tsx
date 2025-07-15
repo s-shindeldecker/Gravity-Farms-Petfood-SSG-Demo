23e60eb9725a5793c5e6cc627f7fc19cd1598a4a
s-shindeldecker <scott.shindeldecker@gmail.com>
2025-07-15 17:04:08 -0500

    Vercel build errors on the typescript

--- src/app/[code]/layout.tsx
+++ src/app/[code]/layout.tsx
@@ -1,8 +1,5 @@
 import { generatePermutations, getPrecomputed } from 'flags/next';
 import { flags } from '@/lib/flags';
-import { UserProvider } from '@/context/UserContext';
-import { AppContent } from '@/components/AppContent';
-import { LaunchDarklyWrapper, LaunchDarklyContextUpdater } from '@/components/LaunchDarklyWrapper';
 
 export async function generateStaticParams() {
   const codes = await generatePermutations(flags);
@@ -16,20 +13,9 @@ export default async function Layout({
   children: React.ReactNode;
   params: Promise<{ code: string }>;
 }) {
-  const { code } = await params;
-  
-  // Decode the precomputed flag values
-  const flagValues = await getPrecomputed(flags, flags, code) as any[];
-  const seasonalBannerText = flagValues[2]; // seasonal-sale-banner-text is the 3rd flag
-
   return (
-    <LaunchDarklyWrapper>
-      <UserProvider>
-        <LaunchDarklyContextUpdater />
-        <AppContent seasonalBannerText={seasonalBannerText}>
-          {children}
-        </AppContent>
-      </UserProvider>
-    </LaunchDarklyWrapper>
+    <>
+      {children}
+    </>
   );
 } 
